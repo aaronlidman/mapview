96b1b7b6ccf7d62910a27ab9b578a0554e799063
Aaron Lidman <aaronlidman@gmail.com>
2017-06-06 15:15:24 -0700

    Generate a default style

--- src/components/map.vue
+++ src/components/map.vue
@@ -25,13 +25,77 @@ module.exports = {
             loadMap();
         });
 
+        function createStyle(layers, sourceId) {
+            layers.forEach(function (layer) {
+                var polygons = {
+                    id: layer.id + '-polygons',
+                    type: 'fill',
+                    source: sourceId,
+                    'source-layer': layer.id,
+                    filter: ['==', '$type', 'Polygon'],
+                    layout: {},
+                    paint: {
+                        'fill-opacity': 0.1,
+                        'fill-color': '#00FF00'
+                    }
+                };
+
+                var polygonOutlines = {
+                    id: layer.id + '-polygon-outlines',
+                    type: 'line',
+                    source: sourceId,
+                    'source-layer': layer.id,
+                    filter: ['==', '$type', 'Polygon'],
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    paint: {
+                        'line-color': '#00FF00',
+                        'line-width': 1,
+                        'line-opacity': 0.75
+                    }
+                };
+
+                var lines = {
+                    id: layer.id + '-lines',
+                    type: 'line',
+                    source: sourceId,
+                    'source-layer': layer.id,
+                    filter: ['==', '$type', 'LineString'],
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    paint: {
+                        'line-color': '#00FF00',
+                        'line-width': 1,
+                        'line-opacity': 0.75
+                    }
+                };
+
+                var points = {
+                    id: layer.id + '-points',
+                    type: 'circle',
+                    source: sourceId,
+                    'source-layer': layer.id,
+                    filter: ['==', '$type', 'Point'],
+                    paint: {
+                        'circle-color': '#00FF00',
+                        'circle-radius': 2.5,
+                        'circle-opacity': 0.75
+                    }
+                };
+
+            });
+        }
+
         function loadMap() {
             map = new mapboxgl.Map({
                 container: 'map',
                 maxZoom: 30
             });
 
-            // we need to make a dumb default style
             map.setStyle('mapbox://styles/mapbox/streets-v9');
         }
     },
