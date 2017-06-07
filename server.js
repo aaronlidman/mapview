80cfab2ab6fcf148e71ba51a4e1584b58e289510
Aaron Lidman <aaronlidman@gmail.com>
2017-06-07 10:02:03 -0700

    Serve vector tiles

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var app = express();
 var log = require('electron-log');
 var homedir = require('os').homedir();
+var MBTiles = require('mbtiles-offline');
+var tiletype = require('@mapbox/tiletype');
 
 app.use(express.static(path.join(__dirname, './')));
 
 var file = require('./src/server/file');
+var loadedTiles = {};
 
 function searchMbtiles(req, res) {
     var dir = req.params.dir ? decodeURIComponent(req.params.dir) : homedir;
@@ -32,10 +34,34 @@ function getMetadata(req, res) {
     });
 }
 
+function getTile(req, res) {
+    var p = req.params;
+    if (!loadedTiles[p.source]) {
+        loadedTiles[p.source] = new MBTiles(decodeURIComponent(p.source));
+    }
+
+    loadedTiles[p.source].findOne([p.x, p.y, p.z].map(Number))
+        .then(function (tile) {
+            if (!tile) {
+                res.writeHead(204);
+                return res.end();
+            }
+            var headers = tiletype.headers(tile);
+            res.writeHead(200, headers);
+            res.end(tile);
+        })
+        .catch(function (err) {
+            console.log(err);
+            log.error(err);
+            res.end();
+        });
+}
+
 module.exports = function (config, callback) {
     app.get('/mbtiles/:dir', searchMbtiles);
     app.get('/mbtiles', searchMbtiles);
     app.get('/metadata/:file', getMetadata);
+    app.get('/:source/:z/:x/:y.:format', getTile);
 
     app.listen(config.port, function () {
         callback(null, config);
