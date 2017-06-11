7521975a6ffee8ee9eb6229282dcce63fab6def6
Aaron Lidman <aaronlidman@gmail.com>
2017-06-11 13:16:38 -0700

    Add navigation to the map component

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,7 +1,15 @@
 <template>
 <div>
+    <table id='navigation' class='collapse left-0 z-max fixed top-3 bold bg-white black'>
+        <tr>
+            <td id='back-button' class='pt3 pb3 pr3 pl3 hover-bg-light-gray pointer' @click.once='backToPicker'>◀</td>
+            <td class='ttu pa3 pr4 ma0 drag'>
+                <h1 id='title' class='ma0'>View</h1>
+            </td>
+        </tr>
+    </table>
     <div id='map' class='no-drag bg-near-black w-100 vh-100'></div>
-    <h4 v-if='shortFile' class='normal top-0 right-0 white code tr ma3 fixed' id='title'>{{ shortFile }}</h4>
+    <h4 v-if='shortFile' class='normal top-0 right-0 white code tr ma2 fixed drag' id='filename'>{{ shortFile }}</h4>
 </div>
 </template>
 
@@ -19,7 +27,7 @@ module.exports = {
         var filepath = encodeURIComponent(hash['/map?file']);
         var map, metadata;
 
-        mapboxgl.accessToken = '';
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25saWRtYW4iLCJhIjoiNTVucTd0TSJ9.wVh5WkYXWJSBgwnScLupiQ';
 
         request('http://localhost:20009/metadata/' + filepath, function (err, resp, body) {
             if (err) return log.error(err);
@@ -37,6 +45,13 @@ module.exports = {
         return {
             shortFile: this.shortFile
         };
+    },
+    methods: {
+        backToPicker: function(event) {
+            this.$router.push({
+                path: '/'
+            });
+        }
     }
 };
 </script>
@@ -44,7 +59,11 @@ module.exports = {
 <style src='../../node_modules/mapbox-gl/dist/mapbox-gl.css'></style>
 
 <style>
-    #title {
+    .top-3 {
+        top: 3rem;
+    }
+
+    #filename {
         font-size: 14px;
         text-shadow: 1px 1px 0px black;
     }
