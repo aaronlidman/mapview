11fd5c2a44405fe1f09f36208087ac59d26d9061
Aaron Lidman <aaronlidman@gmail.com>
2017-06-11 13:17:09 -0700

    Improve picker some more

--- src/components/picker.vue
+++ src/components/picker.vue
@@ -1,32 +1,28 @@
 <template>
 <div>
-    <div class='bg-yellow fixed w-50 vh-100 dt drag'>
-        <div class='dtc v-mid'>
+    <div class='bg-white-50 fixed w-50 vh-100 dt drag'>
+        <div class='dtc v-mid bg-yellow'>
             <h1 class='mb5 white f1 ttu tc'>Pick</h1>
         </div>
     </div>
-    <div id='file-list' class='pa4 w-50 fr bg-white drag' v-show='!loading'>
+    <div id='file-list' class='pa4 w-50 fr bg-near-white drag' v-show='!loading'>
         <div class='dt vh-100 center'>
             <div class='dtc v-mid'>
-                <table>
-                    <tr v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' class='w-100 hover-bg-white pointer'>
-                        <td><span class='fa fa-photo fa-2x'></span></td>
+                <table class='collapse'>
+                    <tr v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' class='w-100 pointer hover-bg-white'>
+                        <td class='pl3'>
+                            <span class='fa fa-photo black-70 fa-fw'></span>
+                        </td>
                         <td class='pa3'>
                             <div>
-                                <span class='filename black'>{{ file.basename }}</span> <span class='black-50 breaky'>in {{ file.dir }}</span>
+                                <span class='filename black'>{{ file.basename }}</span> <span class='black-40 breaky'>in {{ file.dir }}</span>
                             </div>
                             <div>
-                                <span class='black-50'>{{ file.size }}, {{ file.modified }}</span>
+                                <span class='black-40'>{{ file.size }}, {{ file.modified }}</span>
                             </div>
                         </td>
                     </tr>
                 </table>
-
-<!--                 <li v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' class='w-100 pa3 pl4 pr4 hover-bg-white pointer'>
-                    <span class='fa fa-photo'></span><span class='filename black'>{{ file.basename }}</span> <span class='black-50 breaky'>in {{ file.dir }}</span>
-                    <div class='black-50'>{{ file.size }}, {{ file.modified }}</div>
-                </li> -->
-
             </div>
         </div>
     </div>
@@ -35,10 +31,6 @@
 
 <style src='../../node_modules/font-awesome/css/font-awesome.min.css'></style>
 <style>
-.fa {
-    margin-right: 0.5rem;
-}
-
 .breaky {
     word-wrap: break-word;
     overflow-wrap: break-word;
