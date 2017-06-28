057132bf27dca1e1af5164a0041f62fe3d8a6b71
Aaron Lidman <aaronlidman@gmail.com>
2017-06-27 18:25:50 -0700

    Better picker colors

--- src/components/picker.vue
+++ src/components/picker.vue
@@ -5,24 +5,25 @@
             <h1 class='avenir mb5 white f1 ttu tc'>Pick</h1>
         </div>
     </div>
-    <div id='file-list' class='pa3 w-50 fr bg-near-white drag' v-show='!loading'>
+    <div id='file-list' class='pa3 w-50 fr bg-white drag' v-show='!loading'>
         <div class='dt vh-100 center'>
             <div class='dtc v-mid'>
                 <table class='collapse'>
-                    <tr v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' class='w-100 pointer hover-bg-white ba1 b--black-025 bb'>
-                        <td class='pl3 pr3'>
-                            <span v-if="file.format == 'jpg'" class='fa fa-th black-70 fa-fw fa-lr'></span>
-                            <span v-if="file.format == 'jpeg'" class='fa fa-th black-70 fa-fw fa-lr'></span>
-                            <span v-if="file.format == 'png'" class='fa fa-th black-70 fa-fw fa-lr'></span>
-                            <span v-if="file.format == 'pbf'" class='fa fa-map-o black-70 fa-fw fa-lr'></span>
+                    <!-- todo: add sortable headers eventually -->
+                    <tr v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' class='w-100 pointer hover-bg-near-white ba1 b--black-025 bb black'>
+                        <td class='pl3 pr3 black-40'>
+                            <span v-if="file.format == 'jpg'" class='fa fa-th fa-fw fa-lr'></span>
+                            <span v-if="file.format == 'jpeg'" class='fa fa-th fa-fw fa-lr'></span>
+                            <span v-if="file.format == 'png'" class='fa fa-th fa-fw fa-lr'></span>
+                            <span v-if="file.format == 'pbf'" class='fa fa-map-o fa-fw fa-lr'></span>
                         </td>
                         <td class='pr1 pv3'>
-                            <div><span class='black filename'>{{ file.basename }}</span></div>
+                            <div><span class='filename'>{{ file.basename }}</span></div>
                             <div><span class='black-40'> in {{ file.dir }}</span></div>
                         </td>
                         <td class='dtc show-narrow black-40 tr pa1 pv3 v-mid'><span>{{ file.date }}</span></td>
                         <td class='dtc hide-narrow black-40 tr pa1 pv3 v-mid'><span>{{ file.modified }}</span></td>
-                        <td class='black-40 tr pa1 pv3 v-mid pr3'><span>{{ file.size }}</span></td>
+                        <td class='tr pa1 pv3 v-mid pr3 black-40'><span>{{ file.size }}</span></td>
                     </tr>
                 </table>
             </div>
@@ -44,26 +45,11 @@
     font-weight: 500;
 }
 
-.f1 {
-    font-size: 6rem;
-}
-
-.bg-lime {
-    background-color: hsla(90, 70%, 55%, 1);
-}
-
-.bg-new-red {
-    background-color: #D3312B;
-}
-
-.bg-new-yellow {
-    background-color: hsla(45, 87%, 60%, 1);
-}
-
-.bg-newer-yellow {
-    background-color: #FCE933;
-}
-
+.f1 { font-size: 6rem; }
+.bg-lime { background-color: hsla(90, 70%, 55%, 1); }
+.bg-new-red { background-color: #D3312B;}
+.bg-new-yellow { background-color: hsla(45, 87%, 60%, 1); }
+.bg-newer-yellow { background-color: #FCE933; }
 .show-narrow { display: none; }
 .hide-narrow { display: table-cell; }
 
@@ -84,7 +70,6 @@
     .show-narrow { display: table-cell; }
     .hide-narrow { display: none; }
 }
-
 </style>
 
 <script>
