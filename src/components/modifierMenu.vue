95cdb0695ab2208c3b7746065a6f375cafb4076a
Aaron Lidman <aaronlidman@gmail.com>
2017-06-29 16:47:50 -0700

    Add basemap selection UI

--- src/components/modifierMenu.vue
+++ src/components/modifierMenu.vue
@@ -9,17 +9,26 @@
             - todo: select specific color pallettes
             - todo: change background/foreground color
         -->
+        <li class='mb3'>
+            <div class='mb1'>Basemap:</div>
+            <form>
+                <div v-for='map in basemaps' :key='map'>
+                    <input  v-bind:id='map' v-bind:name='map' v-bind:value='map' type='radio' v-model='selectedBasemap'>
+                    <label v-bind:for='map' class='ml1'> {{ map }}</label>
+                </div>
+            </form>
+        </li>
         <li>
             <div class='mb1'>Show:</div>
             <form>
                 <input type='radio' id='none' value='none' v-model='filtered' @change='$emit("update:filter", $event.target.value)'>
-                <label for='none'> all</label><br>
+                <label class='ml1' for='none'> all</label><br>
                 <input type='radio' id='points' value='points' v-model='filtered' @change='$emit("update:filter", $event.target.value)'>
-                <label for='points'> only points</label><br>
+                <label class='ml1' for='points'> only points</label><br>
                 <input type='radio' id='lines' value='lines' v-model='filtered' @change='$emit("update:filter", $event.target.value)'>
-                <label for='lines'> only lines</label><br>
+                <label class='ml1' for='lines'> only lines</label><br>
                 <input type='radio' id='polygons' value='polygons' v-model='filtered' @change='$emit("update:filter", $event.target.value)'>
-                <label for='polygons'> only polygons</label><br>
+                <label class='ml1' for='polygons'> only polygons</label><br>
             </form>
         </li>
     </div>
@@ -32,7 +41,15 @@ module.exports = {
     data: function() {
         return {
             filtered: 'none',
-            show: false
+            show: false,
+            basemaps: [
+                'no basemap',
+                'light',
+                'dark',
+                'satellite',
+                'streets'
+            ],
+            selectedBasemap: 'no basemap'
         }
     }
 }
