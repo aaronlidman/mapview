cd38f19f79eb7152f2cf885cedee19b682245c34
Aaron Lidman <aaronlidman@gmail.com>
2017-07-25 15:41:48 -0700

    Simpler picker

--- src/components/picker.vue
+++ src/components/picker.vue
@@ -1,15 +1,15 @@
 <template>
 <div>
-    <div id='free-space' class='fl fixed bg-blue w-100 vh-100 drag dt'>
+    <div id='free-space' class='fl fixed w-100 vh-100 drag dt bg-white'>
         <globe v-bind:bounds.sync='bounds' v-bind:focus.sync='focus'></globe>
     </div>
-    <div id='file-list' class='fr bg-blue drag absolute right-0 overflow-scroll'>
+    <div id='file-list' class='fr absolute right-0 overflow-scroll vh-100 bg-white'>
         <div @mouseleave='hoverFile(false)' class='w-100 overflow-scroll'>
-            <div v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' @mouseenter='hoverFile(file.path)' class='f6 w-100 hover-bg-white-20 white-50 pointer pa3'>
+            <div v-for='file in files' :key='file.path' @click.once='selectFile(file.path)' @mouseenter='hoverFile(file.path)' class='w-100 hover-bg-black hover-white-60 black-40 pointer pa3 ph35 files'>
                 <div>
-                    <span class='fw-500 white'>{{ file.basename }}</span>
+                    <span class='fw-500 black f5'>{{ file.basename }}</span>
                 </div>
-                <div>
+                <div class='f6'>
                     <span>{{ file.dir }}</span>
                     <div class='w-40 fr nowrap tr'>
                         <div class='w-50 fl ph2'>
@@ -29,7 +29,23 @@
 <style src='../../node_modules/font-awesome/css/font-awesome.min.css'></style>
 <style>
 #free-space { padding-right: 380px; }
-#file-list { width: 380px; }
+#file-list {
+    width: 380px;
+    border-left: 1px solid rgba(0,0,0,0.1);
+}
+.files {
+    border-bottom: 1px solid rgba(0,0,0,0.1);
+    line-height: 1.5;
+}
+
+.ph35 {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+}
+
+.hover-bg-black:hover .black {
+    color: white;
+}
 
 .breaky {
     word-wrap: break-word;
@@ -38,7 +54,7 @@
 }
 
 .bg-hover-lime:hover, .bg-lime { background-color: hsla(90, 70%, 55%, 1); }
-.bg-new-red { background-color: #D3312B;}
+.bg-new-red, .bg-hover-new-red:hover { background-color: hsl(0,66%,50%); }
 .bg-new-yellow { background-color: hsla(45, 87%, 60%, 1); }
 .bg-newer-yellow { background-color: #FCE933; }
 .bg-hover-another-yellow:hover, .bg-another-yellow { background-color: #FEC309; }
@@ -48,7 +64,6 @@
 
 <script>
 var globe = require('./globe.vue');
-var socket = require('socket.io-client')('http://localhost:20009/picker');
 var _ = require('lodash');
 
 module.exports = {
@@ -74,6 +89,7 @@ module.exports = {
     methods: {
         fetchData: function () {
             var that = this;
+            var socket = require('socket.io-client')('http://localhost:20009/picker');
 
             socket.on('connect', function () {
                 that.socket = socket;
@@ -92,11 +108,9 @@ module.exports = {
                 createBboxes(that.files);
             });
 
-            function prepFiles(files, compareTo) {
-                files.forEach(function (file) {
-                    console.log(file.path, file.humanModified);
-                });
+            that.socket = socket;
 
+            function prepFiles(files, compareTo) {
                 if (compareTo) {
                     files = _.uniqWith(compareTo.concat(files), function (value, anotherValue) {
                         return value.path === anotherValue.path;
