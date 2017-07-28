1eb8d35a42d0a0255d944c1b0f9e7aa691a324c9
Aaron Lidman <aaronlidman@gmail.com>
2017-07-28 16:30:56 -0700

    First pass at two package.json structure

---
+++ app/renderer/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var Vue = require('vue');
+var Router = require('vue-router');
+
+var picker = require('../components/picker.vue');
+var map = require('../components/map.vue');
+
+Vue.use(Router);
+
+var router = new Router({
+    routes: [
+        {
+            path: '/',
+            component: picker
+        },
+        {
+            path: '/map',
+            component: map
+        }
+    ]
+});
+
+new Vue({
+    router: router
+}).$mount('#app');
